20eddf21a48bc13658d4c990ae4a5f0070f8dfad
Daniel Ozotha <oreziter@gmail.com>
2024-03-10 13:16:17 -0500

    sprint6 challenge

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Character from './Character'
-
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
-
-function App() {
+import Character from './Character';
+ 
+   function App() {
+    const [ characters, setCharacters ] = useState ([]);
+    const [planets, setPlanets] = useState({});
+   
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+  useEffect(() => {
+    const fetchData = async () => {
+     try {
+      const [peopleResponse, planetsResponse] = await Promise.all([
+        axios.get('http://localhost:9009/api/people'),
+
+        axios.get('http://localhost:9009/api/planets'),
+      ]);
+   
+      setCharacters(peopleResponse.data);
+      setPlanets(planetsResponse.data);
+ 
+     } catch (error) {
+      console.error('Error fetching data:', error);
+      
+     }
+    };
+
+    fetchData();
+  }, [])
+const getPlanet = (homeworld) => {
+  const planet = planets.find(pt => pt.id === homeworld).name;
+  return planet;
+}
   return (
-    <div>
+    <div className='App'>
+   
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
+      
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+  characters.map((item) => <Character key={item.id} character={item} planet={getPlanet(item.homeworld)}/>)
+      }
     </div>
-  )
-}
+  );
+ 
+  }
 
-export default App
-
+  export default App;
 // ❗ DO NOT CHANGE THE CODE  BELOW
+
 if (typeof module !== 'undefined' && module.exports) module.exports = App
+
+
+
+// const PeopleResponse = await axios.get ('http://localhost:9009/api/people');
+//   console.log(PeopleResponse.data);
+
+// const PlanetsResponse = await axios.get ('http://localhost:9009/api/planets');
+//    console.log(PlanetsResponse.data);
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react' 
 
-function Character() { // ❗ Add the props
+function Character({ character, planet }) { // ❗ Add the props
+  const [showHomeworld, setShowHomeWorld] = useState (false);
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+   const toggleHomeworld = () => {
+    setShowHomeWorld(prevState => !prevState);
+   };
+
   return (
-    <div>
+    <div className="character-card" onClick={toggleHomeworld}>
+      <h2 className='character-name'>{character.name}</h2>
+      {showHomeworld && (
+       <p className='character-planet'>Planet: {planet}</p> 
+      )}
       {/* Use the same markup with the same attributes as in the mock */}
     </div>
-  )
+  );
 }
 
-export default Character
+export default Character;
